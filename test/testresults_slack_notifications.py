84bdd60b9bb29e9c79cedd85798f69796f3c1815
Swapna Pothana <spothana@snaplogic.con>
2025-07-25 09:59:57 -0700

    Fix: Updated slack notification webhook

--- test/testresults_slack_notifications.py
+++ test/testresults_slack_notifications.py
@@ -9,7 +9,7 @@ from robot.api import ExecutionResult
 from tabulate import tabulate
 
 # SLACK_WEBHOOK_URL = "https://hooks.slack.com/services/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"
-SLACK_WEBHOOK_URL = "https://hooks.slack.com/services/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"
+SLACK_WEBHOOK_URL = "https://hooks.slack.com/services/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"
 
 # Global variables for commit information
 commit_url = None
